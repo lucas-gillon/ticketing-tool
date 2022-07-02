edc96ab1fd3806803d9c7becfdeb2bb6bf4c67f5
Lucas Gillon <95145038+lucas-gillon@users.noreply.github.com>
2022-07-02 18:25:13 +0200

    ajout de react-bootstrap

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 // import { useUser } from "@auth0/nextjs-auth0";
 import Head from "next/head";
-import Link from "next/link";
+import Navbar from "react-bootstrap/Navbar";
 import { ReactNode } from "react";
 
 const Layout: React.FC<{ children: any }> = ({
@@ -9,25 +9,12 @@ const Layout: React.FC<{ children: any }> = ({
   children: ReactNode;
 }) => {
   return (
-    <div className="Background">
+    <div>
       <Head>
-        <title>NotDiscord</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav>
-        <header className="p-3 bg-primary  text-white">
-          <div className="container">
-            <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-              <Link href="/">
-                <a className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                  NotDiscord
-                </a>
-              </Link>
-              <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"></ul>
-            </div>
-          </div>
-        </header>
-      </nav>
+      <Navbar>
+      <title>NotDiscord</title>
+      </Navbar>
       {children}
       <footer>
         <hr />
